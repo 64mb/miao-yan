3691e498fecfb72fa4cfc1d1b1709d6731110760
Georgy <i@64mb.ru>
2026-09-09 02:09:03 +0300

    release: prepare fork V4.2.6

--- MiaoYanAndroid/app/build.gradle.kts
+++ MiaoYanAndroid/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.tw93.miaoyan.android"
         minSdk = 35
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.1.5-prototype"
+        versionCode = 7
+        versionName = "0.1.6-prototype"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
